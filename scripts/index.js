b24036a8d2d81b41879eb097f9e924bc34fd92d5
Ruslan T <Ruslan85tim@yandex.kz>
2024-08-01 00:54:40 +0500

    проект 5 версия 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
 
+const placesList = document.querySelector(".places__list");
+
 // @todo: Функция создания карточки
 
+function createCard(card, { deleteCard }) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+  cardImage.alt = card.name;
+  cardImage.src = card.link;
+  cardTitle.textContent = card.name;
+  cardDeleteButton.addEventListener("click", function () {
+    deleteCard(cardElement);
+  });
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
 
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (card) {
+  const cardElement = createCard(card, { deleteCard });
+  placesList.append(cardElement);
+});
